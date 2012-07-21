a881009151b7ec7b25a70d46172864a1f96f453c
Fang Nan <nanfang05@gmail.com>
2012-07-21 21:26:25 +0800

    use right copyright

--- SQLMigrate/SQLMigrate.h
+++ SQLMigrate/SQLMigrate.h
@@ -3,7 +3,7 @@
 //  SQLMigrate
 //
 //  Created by nanfang on 7/21/12.
-//  Copyright (c) 2012 tukeQ.com. All rights reserved.
+//  Copyright (c) 2012 lvtuxiongdi.com All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
