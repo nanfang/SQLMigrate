02c81ee363d3c3f7e185c27f7548f8ab314a5dde
Fang Nan <nanfang05@gmail.com>
2012-07-29 00:13:34 +0800

    make migration runnable

--- SQLMigrate/SQLMigrate.m
+++ SQLMigrate/SQLMigrate.m
@@ -8,27 +8,66 @@
 
 #import "SQLMigrate.h"
 #import "DBUtils.h"
+#import "NSString+Ext.h"
+static NSString * const MIGRATE_PATH = @"";
+static NSString * const MIGRATE_EXT = @"migrate.sql";
 
 @implementation SQLMigrate
++ (NSString*) fileName:(NSString*)filePath
+{
+    return [filePath substringFromIndex:[filePath lastIndexOf:@"/"]+1];  
+}
+
++ (int) versionOfFile:(NSString*)filePath
+{
+    NSString *s = [self fileName:filePath]; 
+    s = [s substringToIndex:[s indexOf:@"_"]];
+    return [s intValue];
+}
 
 + (void)migrate:(FMDatabase*)db
 {
-    int version = [DBUtils dbVersion:db];
-    NSLog(@"current db version=%d", version);
-    // TODO migrate from version
-    
-    // TODO update version
-    int new_version= version;
-    NSArray* sqls = [[NSBundle mainBundle] pathsForResourcesOfType:@"sql" inDirectory:@""];
-    for(NSString* sql in sqls){
-        NSLog(@"%@", sql);
+    [db beginTransaction];
+    int current_version = [DBUtils dbVersion:db];
+    int new_version= current_version;
+    NSArray* sqls = [[NSBundle mainBundle] pathsForResourcesOfType:MIGRATE_EXT inDirectory:MIGRATE_PATH];
+    [sqls sortedArrayUsingComparator:^(NSString *s1, NSString *s2){
+        return [self versionOfFile:s1] - [self versionOfFile:s2];
+    }];
+    @try{
+        for(NSString* sql in sqls){
+            int version = [self versionOfFile:sql];
+            if (version > current_version) {
+                NSArray* clauses =[[NSString stringWithContentsOfFile:sql encoding:NSUTF8StringEncoding error:nil]componentsSeparatedByString:@";"];
+                for(NSString *c in clauses){
+                    NSString *clause = [c stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]]; 
+                    if([clause isEqual:@""]){
+                        continue;
+                    }
+                    if([db executeUpdate:clause]){
+                        new_version = version;                
+                    }else {
+                        [NSException raise:@"Fail to migrate" format:@"error to run %@", [self fileName:sql]];
+                    }
+                }
+                NSLog(@"executed %@", [self fileName:sql]);
+            }
+        }
+        if(new_version>current_version){
+            [db executeUpdate:[NSString stringWithFormat:@"PRAGMA user_version=%d", new_version]];
+            NSLog(@"db is updated to version=%d", [DBUtils dbVersion:db]);
+        }else {
+            NSLog(@"db is already up to date, version=%d", [DBUtils dbVersion:db]);
+        }
+        [db commit];
+        NSLog(@"migrate done");
+    }@catch(NSException *exception){
+        NSLog(@"%@: %@", [exception name], [exception reason]);
+//        [db rollback];
+        NSLog(@"migrate abort");
     }
-        
-    if(new_version>version){
-        [db executeUpdate:[NSString stringWithFormat:@"PRAGMA user_version=%d", new_version]];
-        NSLog(@"updated to version=%d", [DBUtils dbVersion:db]);
-    }
-}
 
+
+}
 
 @end
